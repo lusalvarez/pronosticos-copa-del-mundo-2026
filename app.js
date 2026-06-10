f6ec2f5d26560119fe0a90a9d56d3602516e4c34
Laurent Lusseau <lusseaul@fr.ibm.com>
2026-06-10 17:06:27 +0200

    Simplification classement WhatsApp: suppression marcadores exactos

--- app.js
+++ app.js
@@ -1218,7 +1218,7 @@ function generateWhatsAppSummary(dayIndex = 0) {
   ranking.forEach((participant, index) => {
     const medals = ["🥇", "🥈", "🥉"];
     const medal = index < 3 ? medals[index] : `${index + 1}.`;
-    text += `${medal} ${participant.name}: ${participant.totalPoints}pts (${participant.exactScores}⚽${participant.correctFirstGoals}🎯)\n`;
+    text += `${medal} ${participant.name}: ${participant.totalPoints}pts\n`;
   });
   
   text += "\n⚽ *PARTIDOS*\n";
